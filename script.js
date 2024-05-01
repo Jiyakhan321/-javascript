e5c9d22d680a68cc90d2a47b5f090004483a0122
Jiyakhan321 <168368019+Jiyakhan321@users.noreply.github.com>
2024-05-01 15:53:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("careful writer ends every declarative sentence with a period");
+
+alert("Welcome to our website");
+var first_name = "Jaweria";
+console.log(first_name);
+var last_name = 'Khan';
+console.log(last_name);
+
+var number = 2021
+console.log(number);
+var text = "It is invented in 1845";
+console.log(text);
+
+var name = "Jaweria";
+var email = "Jaweria@gmail.com";
+var dateofBirth = 2004;
+console.log(name + "   " + email + "   " + dateofBirth);
+console.log(email);
+console.log(dateofBirth);

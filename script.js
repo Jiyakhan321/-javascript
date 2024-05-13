02097fef52369c01b2b0fca501a805b01e15d4c8
Jiyakhan321 <168368019+Jiyakhan321@users.noreply.github.com>
2024-05-13 15:25:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ let a = 12;
 let b = 20;
 let result = a + b;
 console.log(" The sum of two number is : " + (a + b));
+let username = prompt("your name is?")
+console.log(username);
+
+let number1 = +prompt("enter your number" ,92);
+let number2 = +prompt("enter your second number");
+console.log(number1 * number2);
+
+let x = 21;
+x += 4;
+x = x % 4;
+console.log(x);
+
+let y = 12;
+let z = 18;
+console.log(y!==z);
+
+let mode = "dark";
+if (mode == "dark") {
+    color = "red";
+}
+console.log(color);
+
+let age = +prompt("Enter your age");
+if (age > 18) {
+    console.log("You are eligible for voting");
+}
+else{
+    console.log("You are not eligible for voting");
+}

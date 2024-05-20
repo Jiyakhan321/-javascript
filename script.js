cd0bf10c9f8fea0149dda48bc5bf5f35444d7db1
Jiyakhan321 <168368019+Jiyakhan321@users.noreply.github.com>
2024-05-20 15:59:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,83 @@ else if(age > 60){
 else{
     console.log("middle");
 }
+// let weight = +prompt("Enter weight");
+// let time = +prompt("Enter time");
+
+// if (weight > 300 && time < 6 ) {
+
+//     //!(444 > 300 || 4 < 6)
+//     alert("Come to our tryout!");
+// }
+// else{
+//     alert("Come to our cookout!");
+// }
+
+
+// let x = 10;
+// let y = 5;
+
+// if (x > 5) {
+//     if (y > 2) {
+//         console.log("Both x and y are greater than their respective thresholds.");
+//     } else {
+//         console.log("x is greater than 5, but y is not greater than 2.");
+//     }
+// } else {
+//     console.log("x is not greater than 5, so the inner condition is not checked.");
+// }
+
+// let temperature = 15;
+// let isSunny = true;
+
+// if(temperature > 25){
+//   console.log("It's hot outside!");
+
+//   if(isSunny){
+//       console.log("Don't forget to wear sunscreen.");
+//   }
+//   else{
+//     console.log("You might still want to wear sunscreen.");
+//   }
+// }
+// else if(temperature >= 15 && temperature <= 25){
+//     console.log("It's a pleasant day.");
+//   if(isSunny){
+//     console.log("Enjoy the sunshine!");
+//   }
+//   else{
+//     console.log("Even if it's cloudy, it's still nice outside.");
+// }
+// }
+// else{
+//     console.log("It's a bit chilly.");
+//     if (isSunny) {
+//         console.log("Don't be fooled by the sun, it's still cool.");
+//     } else {
+//         console.log("It might be cloudy, so bring a jacket just in case.");
+//     }
+// }
+
+
+ 
+var textpara = (" " + "Pizza" + "\n" + " " + "Pizz" + "\n" + " " + "Piz" + "\n" + " " + "Pi" + "\n"  + " " +  "P");
+ 
+alert(textpara);
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+var email = "My email adress is : Jaweria@gmail.com";
+alert(email);
+
+
+var book = " I am trying to learn from the book A smarter way to learn JavaScript.";
+
+alert(book);
+
+var username = "John Doe";
+alert(username);
+
+var message = "Hello World";
+alert(message);
+
